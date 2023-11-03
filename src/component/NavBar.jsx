44fe734b1d242eb68ece02b114297c79bc074629
Ruzeda <xmalone0920@gmail.com>
2023-11-03 13:45:43 -0700

    NavBar css

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom"
 
 const NavBar = () => {
     return (
-        <div>NavBar</div>
+        <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
+        <h1 className="text-xl font-extrabold"><Link to='/'>Blog Market</Link></h1>   
+        <div className="flex justify-center items-center space-x-0">
+            <p></p>
+        </div>
+        <div className='flex items-center justify-center space-x-2 md:space-x-4'></div>
+        <h3><Link to='/login'>Login</Link></h3>
+        <h3><link to='/register'>Register</link></h3>
+
+            
+        </div>
     )
 }
 
