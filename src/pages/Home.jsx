0ae04690d4488307611ad96338aed67cff8d5baa
Ruzeda <xmalone0920@gmail.com>
2023-11-03 16:05:12 -0700

    wip

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,11 @@ import HomePosts from "../component/HomePost"
 
 const Home = () => {
     return (
-     <div>
+     <div className="px-8 md:px-[200px]">
+       <HomePosts />
+       <HomePosts />
+       <HomePosts />
+       <HomePosts />
        <HomePosts />
      </div>
     )
