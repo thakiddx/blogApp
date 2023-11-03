fe8121f7944556818b3b63a08b24c9c235c403fa
Ruzeda <xmalone0920@gmail.com>
2023-11-03 16:49:47 -0700

    footer wip

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,9 +1,29 @@
 
 const Footer = () => {
     return (
-     <div>
-        Footer
-     </div>
+    <>
+        <div className="mt-8 w-full bg-black px-8 md:px-[500px] flex justify-between text-sm md:text-md py-8 ">
+        <div className="flex flex-col text-white">
+            <p>Featured Blogs</p>
+            <p>Most Viewed</p>
+            <p>Readers Choice</p>
+        </div>
+
+        <div className="flex flex-col text-white">
+            <p>Forum</p>
+            <p>Support</p>
+            <p>Recent Posts</p>
+        </div>
+
+        <div className="flex flex-col text-white">
+            <p>Privacy Policy</p>
+            <p>About Use</p>
+            <p>Terms & Conditions</p>
+            <p>Terms of Service</p>
+        </div>
+        </div>
+        <p className="py-2 pb-2 text-center text-white bg-black">All rights reserved @Blog Market 2023</p>
+    </>
     )
 }
 
