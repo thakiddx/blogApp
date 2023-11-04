3ad4adf0817599604780b0ee8ebfc4e5b7222dde
Ruzeda <xmalone0920@gmail.com>
2023-11-03 17:10:27 -0700

    wip

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,7 +2,7 @@
 const Footer = () => {
     return (
     <>
-        <div className="mt-8 w-full bg-black px-8 md:px-[500px] flex justify-between text-sm md:text-md py-8 ">
+        <div className="mt-8 w-full bg-black px-8 md:px-[500px] flex md:flex-row flex-col space-y-4 md:space-y-0  items-start md:justify-between text-sm md:text-md py-8 ">
         <div className="flex flex-col text-white">
             <p>Featured Blogs</p>
             <p>Most Viewed</p>
