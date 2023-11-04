e34ef513bccfd4b765383aae9757938299154ab0
Ruzeda <xmalone0920@gmail.com>
2023-11-03 18:42:37 -0700

    navbar wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { Routes, Route } from "react-router-dom"
-import Footer from "./component/Footer"
-import NavBar from "./component/NavBar"
 import Home from "/home/xmalone0920/blogApp/frontend/src/pages/Home.jsx"
 import Register from "./pages/Register"
 import Login from "./pages/Login"
@@ -8,13 +6,11 @@ import Login from "./pages/Login"
 function App() {
   return (
     <div>
-      <NavBar />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/login" element={<Login/>} />
         <Route exact path="/register" element={<Register />} />
       </Routes>
-      <Footer />
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,23 @@
+import Footer from "../component/Footer"
 import HomePosts from "../component/HomePost"
+import NavBar from "../component/NavBar"
 
 
 const Home = () => {
     return (
-     <div className="px-8 md:px-[200px]">
-       <HomePosts />
-       <HomePosts />
-       <HomePosts />
-       <HomePosts />
-       <HomePosts />
-     </div>
+    <>
+    <NavBar/>
+      <div className="px-8 md:px-[200px]">
+        <HomePosts />
+        <HomePosts />
+        <HomePosts />
+        <HomePosts />
+        <HomePosts />
+      </div>
+      <Footer/>
+    </>
+
+    
     )
 }
 
