540519eef386090f5d9a3dd1b4e1162d93f5e00a
Ruzeda <xmalone0920@gmail.com>
2023-11-05 01:54:52 -0700

    profile page wip

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,19 @@
+import NavBar from '../component/NavBar'
+import Footer from '../component/Footer'
+
+const Profile = () => {
+  return (
+    <div>
+      <NavBar/>
+      <div className='px-8 md:px-[200px] mt-8 flex md:flex-row flex-col-reverse'>
+        <div className='flex flex-col md:w-[70%] w-full'>
+
+        </div>
+        <div></div>
+      </div>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Profile
