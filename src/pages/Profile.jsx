a7eac8139b0d9e1221021be063048d79fab7bfcc
Ruzeda <xmalone0920@gmail.com>
2023-11-05 01:54:40 -0800

    profile post & profile wip

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import NavBar from '../component/NavBar'
 import Footer from '../component/Footer'
+import ProfilePosts from '../component/ProfilePosts'
 
 const Profile = () => {
   return (
@@ -7,9 +8,19 @@ const Profile = () => {
       <NavBar/>
       <div className='px-8 md:px-[200px] mt-8 flex md:flex-row flex-col-reverse'>
         <div className='flex flex-col md:w-[70%] w-full'>
-
+            <h1 className='text-xl font-bold mb-4'>Your posts:</h1>
+            <ProfilePosts/>
         </div>
-        <div></div>
+        <div className='flex flex-col space-y-4 md:w-[30%] w-full'>
+            <h1 className='text-xl font-bold mb-4'>Profile</h1>
+            <input className='outline-none px-4 py-2 text-gray-500' placeholder='Your username' type='text'/>
+            <input className='outline-none px-4 py-2 text-gray-500' placeholder='Your email' type='email'/>
+            <input className='outline-none px-4 py-2 text-gray-500' placeholder='Your password' type='password'/>
+            <div className='flex items-center space-x-4 mt-8'>
+                <button className='text-white font-semibold bg-black px-4 py-2 hover:text-black hover:bg-gray-400'>Update</button>
+                <button className='text-white font-semibold bg-black px-4 py-2 hover:text-black hover:bg-gray-400'>Delete</button>
+            </div>
+        </div>
       </div>
       <Footer/>
     </div>
