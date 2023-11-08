d45bd292e363bb9b9eab75d4561e3b96efea8214
Ruzeda <xmalone0920@gmail.com>
2023-11-07 16:15:16 -0800

    wip1

---
+++ backend/backend/models/Post.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const PostSchema = new mongoose.Schema({
+    title:{
+        type:String,
+    },
+    desc:{
+        type:String,
+
+    },
+    img:{
+        type:String,
+        
+    },
+    // username:{
+    //     type:String,
+    //     required:true,  
+    // },
+    // userId:{
+    //     type:String,
+    //     required:true,  
+    // },
+    // categories:{
+    //     type:Array,
+        
+    // },
+},{timestamps:true})
+const Post = mongoose.model("Post",PostSchema);
+module.exports = Post
